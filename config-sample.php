24d6809135735ca42f02311ff7d528289798b410
conejoninja <conejo@theninjabunny.com>
2014-04-19 18:14:44 +0200

    first commit, moved from Osclass core to its own repo

---
+++ config-sample.php
@@ -0,0 +1,20 @@
+<?php
+
+define('TEST_SERVER_URL', 'http://localhost/Osclass/');
+
+define('TEST_DB_HOST', 'localhost');
+define('TEST_DB_USER', 'root');
+define('TEST_DB_PASS', '');
+define('TEST_DB_NAME', 'osclass_test_db');
+
+define('TEST_ADMIN_USER', 'admin');
+define('TEST_ADMIN_PASS', 'admin');
+define('TEST_ADMIN_EMAIL', 'test@example.com');
+
+define('TEST_USER_USER', 'testname');
+define('TEST_USER_PASS', 'testing');
+define('TEST_USER_EMAIL', 'user@example.com');
+
+define('TEST_IMAGE_PATH', '');
+define('TEST_IMAGE_URL', '');
+
---
+++ scripts/git.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: conejo
+ * Date: 4/19/14
+ * Time: 1:25 PM
+ */ 
